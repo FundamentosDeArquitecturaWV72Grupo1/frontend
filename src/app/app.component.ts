0083991f349ff5992aaade6a2d9f69c329796d71
MiguelC <sackito992@gmail.com>
2024-10-30 18:11:41 -0500

    feat: fix main color and index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
 
 @Component({
@@ -11,4 +11,23 @@ import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
 })
 export class AppComponent {
   title = 'MathPlayOpen';
+  showSidebar: boolean = false;
+  load:any =null
+  ngOnInit():void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // @ts-ignore
+        this.showSidebar = event.url !== '/sign-up' && event.url !== '/sign-in' && event.url !== '/password-recovery' && event.url !== '/' && event.url !== '/login/:id/:role';
+        if (this.showSidebar) {
+          this.load=1;
+        }else {
+          this.load=0;
+        }
+      }
+    });
+
+  }
+  constructor(private router: Router) {
+
+  }
 }
