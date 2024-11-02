5e789bda81eb7dfcc9ea291e1592531223977fc2
MiguelC <sackito992@gmail.com>
2024-11-01 22:36:56 -0500

    feat: fix errors with page profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {UserSigninComponent} from './mathplayopen/components/user-signin/user-signin.component';
 import {UserSignupComponent} from './mathplayopen/components/user-signup/user-signup.component';
 import {NewsComponent} from './mathplayopen/pages/news/news.component';
+import {ProfileComponent} from './mathplayopen/pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: 'news', component: NewsComponent},
   { path: 'sign-up', component: UserSignupComponent},
   { path: 'sign-in', component: UserSigninComponent},
+  {path: 'profile', component: ProfileComponent},
   { path: '', redirectTo: 'sign-in', pathMatch: 'full'}
 ];
--- src/app/mathplayopen/pages/profile/profile.component.ts
+++ src/app/mathplayopen/pages/profile/profile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import {User} from '../../models/user.entity';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  user: User = new User(1, 'username', 'Full Name', 'user@example.com', '', 'User'); // Ejemplo de datos iniciales
 
+  constructor() {}
+
+  onSubmit() {
+    console.log('User data submitted:', this.user);
+
+  }
 }
